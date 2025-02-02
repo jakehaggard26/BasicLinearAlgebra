ed6416c2ec64526adb93bcc5ea8eda13c24004b8
Jake <jhaggard26@gmail.com>
2025-02-02 10:03:11 -0500

    Add Clustering Example

--- Examples/simple_clustering.py
+++ Examples/simple_clustering.py
@@ -10,18 +10,18 @@ num_vectors = 15000
 #N = [Vector(np.random.randint(-10,10,n)) for x in range(num_vectors)]
 N = [
     # Age, Income, Credit Score
-    Vector([26, 36500, 427]), # "Poor" person
-    Vector([29, 45000, 562]), # "Poor" person
-    Vector([42, 98000, 697]), # "Rich" person
-    Vector([27, 58000, 625]), # "Poor" person
-    Vector([45, 136500, 654]), # "Rich" person
-    Vector([24, 22500, 459]), # "Poor" person
+    Vector([26, 36500, 427]), # "Poor", "young" person
+    Vector([29, 45000, 562]), # "Poor", "young" person
+    Vector([27, 58000, 625]), # "Poor", "young" person
+    Vector([24, 22500, 459]), # "Poor", "young" person
     
-    Vector([48, 125000, 671]), # "Rich" person
-    Vector([49, 97500, 725]), # "Rich" person
+    Vector([48, 125000, 671]), # "Rich", "middle-aged" person
+    Vector([45, 136500, 654]), # "Rich", "middle-aged" person
+    Vector([49, 97500, 725]), # "Rich", "middle-aged" person
+    Vector([42, 98000, 697]), # "Rich", "middle-aged" person
 
-    Vector([56, 1250000, 750]), # "Super Rich" person
-    Vector([59, 1500000, 750]) # "Super Rich" person
+    Vector([56, 1250000, 750]), # "Super Rich", "old" person
+    Vector([59, 1500000, 750]) # "Super Rich", "old" person
 ]
 
 # number of clusters
